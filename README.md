55d03ea2b984df6f47aedafa1a2156bdd94c3069
angus <lothian@kth.se>
2017-01-22 17:32:10 +0100

    Add update indaScript2.0

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A python script for making it easier to view students code during the Inda-class at KTH.
 
-To run first start setup.py, and then indaScript.py.
+To run just launch indaScript.py :).
--- code/indaScript.py
+++ code/indaScript.py
@@ -1,11 +1,8 @@
 import random
 import webbrowser
 
-weekFile = open("week.txt", "r")
-week = weekFile.read()
-weekFile.close()
-
-print(week)
+print("Which week is it?")
+week = input()
 
 studentFile = open("students.txt", "r")
 
@@ -14,14 +11,30 @@ for line in studentFile:
     student = line.strip('\n').split(' ')
     studentList.append(student)
 
-selectedStudent = studentList[random.randint(0, len(studentList)-1)]
-print(selectedStudent)
-git = selectedStudent[0]
-firstName = selectedStudent[1]
-lastName = selectedStudent[2]
+coolRunnings = True
 
-print(firstName, lastName, "has been selected")
+while coolRunnings:
+    print("Select a student? y/n")
+    answer = input().lower()
+    if answer == "y":
+        if len(studentList) == 0:
+            print("Sorry yer out of students :^)")
+            coolRunnings = False
 
-url = "https://gits-15.sys.kth.se/inda-16/" + git  + "-week-" + week + "/"
+        else:
+            selectedStudent = studentList.pop(random.randint(0, len(studentList)-1))
+            git = selectedStudent[0]
+            firstName = selectedStudent[1]
+            lastName = selectedStudent[2]
 
-webbrowser.open(url)
+            print(firstName, lastName, "has been selected")
+
+            url = "https://gits-15.sys.kth.se/inda-16/" + git  + "-week-" + week + "/"
+            webbrowser.open(url)
+
+    elif answer == "n":
+        print("Then my purpose is finito")
+        coolRunnings = False
+
+    else:
+        print("Mr robato no understato")
